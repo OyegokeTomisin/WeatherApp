7154746e4458ac957ab414805d8154e9977c6e37
tomi <tomisin18@gmail.com>
2021-02-13 08:59:39 +0100

    Add weather enum

---
+++ WeatherApp/Weather.swift
@@ -0,0 +1,14 @@
+//
+//  Weather.swift
+//  WeatherApp
+//
+//  Created by OYEGOKE TOMISIN on 13/02/2021.
+//
+
+import Foundation
+
+enum Weather {
+    case rainy(scene: Scene)
+    case sunny(scene: Scene)
+    case cloudy(scene: Scene)
+}
