67a6eb3c5806d0d06dd3ce49fe2d644401f94d1a
tomi <tomisin18@gmail.com>
2021-02-13 09:49:49 +0100

    Add open weather api key

--- WeatherApp/Environment.swift
+++ WeatherApp/Environment.swift
@@ -10,4 +10,13 @@ import Foundation
 enum Environment {
     case production
     case development
+
+    var openWeatherAPIKey: String {
+        switch self {
+        case .production:
+            return "f2160a8f1f25f327507c8d6e9ad9b7fa"
+        case .development:
+            return "f2160a8f1f25f327507c8d6e9ad9b7fa"
+        }
+    }
 }
