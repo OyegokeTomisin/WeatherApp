c272044ed7ffdac8dfae092d95bfde29c6d4e829
tomi <tomisin18@gmail.com>
2021-02-13 09:44:31 +0100

    Add environment variables

---
+++ WeatherApp/Environment.swift
@@ -0,0 +1,13 @@
+//
+//  Environment.swift
+//  WeatherApp
+//
+//  Created by OYEGOKE TOMISIN on 13/02/2021.
+//
+
+import Foundation
+
+enum Environment {
+    case production
+    case development
+}
