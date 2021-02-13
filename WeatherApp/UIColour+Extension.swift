da59028bd3866ef2c1941ca88522ad813cd845fe
tomi <tomisin18@gmail.com>
2021-02-13 08:56:02 +0100

    Add app colours to UIColour extension

---
+++ WeatherApp/UIColour+Extension.swift
@@ -0,0 +1,27 @@
+//
+//  UIColour+Extension.swift
+//  WeatherApp
+//
+//  Created by OYEGOKE TOMISIN on 13/02/2021.
+//
+
+import UIKit
+
+extension UIColor {
+
+    class var cloudy: UIColor! {
+        return UIColor(named: "cloudy")
+    }
+
+    class var rainy: UIColor! {
+        return UIColor(named: "rainy")
+    }
+
+    class var sunnyForest: UIColor! {
+        return UIColor(named: "sunny_forest")
+    }
+
+    class var sunnySea: UIColor! {
+        return UIColor(named: "sunny_sea")
+    }
+}
