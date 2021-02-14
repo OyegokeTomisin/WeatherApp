7e221a007092f26648023c554be145382ff1f253
tomi <tomisin18@gmail.com>
2021-02-14 20:45:39 +0100

    Add weather result view controller

---
+++ WeatherApp/WeatherResultViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherResultViewController.swift
+//  WeatherApp
+//
+//  Created by OYEGOKE TOMISIN on 14/02/2021.
+//
+
+import UIKit
+
+final class WeatherResultViewController: UIViewController {
+
+    @IBOutlet weak private var resultTableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
