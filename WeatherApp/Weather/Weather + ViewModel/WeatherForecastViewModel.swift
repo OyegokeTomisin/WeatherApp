e12eb3bf97c622dddefb945691b16113338d5dd4
tomi <tomisin18@gmail.com>
2021-02-15 13:52:06 +0100

    Display forecast date on UI

--- WeatherApp/Weather/Weather + ViewModel/WeatherForecastViewModel.swift
+++ WeatherApp/Weather/Weather + ViewModel/WeatherForecastViewModel.swift
@@ -11,11 +11,27 @@ struct WeatherForecastViewModel {
 
     let weather: Weather
 
+    let tempratureData: TemperatureData?
+    let date: String?
+
     var day: String {
-        return "Tuesday"
+        guard let date = date else { return "N/A" }
+        return formatDate(date)
     }
 
     var temprature: String {
-        return String(18)
+        return String(tempratureData?.temp ?? 0)
+    }
+
+    private func formatDate(_ date: String) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        dateFormatter.timeZone = TimeZone(abbreviation: "UTC")
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+
+        guard let date = dateFormatter.date(from: date) else { return "" }
+        dateFormatter.dateFormat = "EEEE"
+        dateFormatter.timeZone = NSTimeZone.local
+        return dateFormatter.string(from: date)
     }
 }
